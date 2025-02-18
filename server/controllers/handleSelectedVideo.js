f99cec8fe4790ead98e59cad7025df7163a15d25
Banoth Sujith <banothsujith4@gmail.com>
2025-02-18 11:37:19 +0530

    Update handleSelectedVideo.js

--- server/controllers/handleSelectedVideo.js
+++ server/controllers/handleSelectedVideo.js
@@ -1,26 +1,9 @@
-const handleSelectedVideo = async (req, res) => {
-  const videoId = req.params.video;
-  const userId = req.user?.id;
-  if (!videoId) {
-    return res.status(400).json({ message: "Invalid request" });
-  }
-
-  try {
-    // Increment the view count
-    await Blog.findByIdAndUpdate(videoId, { $inc: { views: 1 } });
-
-    const result = await Blog.aggregate([
-      { $match: { _id: new mongoose.Types.ObjectId(videoId) } },
-      {
-        $lookup: {
-          from: 'users',
-          localField: 'createdBy',
-          foreignField: '_id',const Blog = require('../models/blog');
+const Blog = require('../models/blog');
 const mongoose = require('mongoose');
 
 const handleSelectedVideo = async (req, res) => {
   const videoId = req.params.video;
-  const userId = req.user?.id;
+  const userId = req.user?.id || [];
   if (!videoId) {
     return res.status(400).json({ message: "Invalid request" });
   }
@@ -44,7 +27,7 @@ const handleSelectedVideo = async (req, res) => {
       {
         $addFields:{
           isSubscribed:{
-            $in:[userId,{$ifNull:["$creator.subscribers", []]}]
+            $in:[userId,{$ifNull : ["$creator.subscribers", []]}]
           }
         }
       },
@@ -58,7 +41,7 @@ const handleSelectedVideo = async (req, res) => {
       {
         $addFields:{
           isUnliked:{
-            $in:[userId,{$ifNull : ["$unlikes",[]]}]
+            $in:[userId,{$ifNull:["$unlikes",[]]}]
           }
         }
       },
@@ -73,71 +56,13 @@ const handleSelectedVideo = async (req, res) => {
           userName: '$creator.userName',
           ownerId:'$creator._id',
           profile: '$creator.profile',
-          subscribersCount: { $size: '$creator.subscribers' },
+          subscribersCount: { $size:{$ifNull:['$creator.subscribers',[]]} },
           isSubscribed: 1,
           isliked:1,
           isUnliked:1,
-          likeCount:{$size:"$likes"},
-          dislikeCount:{$size:"$unlikes"},
+          likeCount:{$size:{$ifNull:["$likes", []]}},
+          dislikeCount:{$size:{$ifNull:["$unlikes",[]]}},
            },
-      },
-    ]);
-
-    if (!result || result.length === 0) {
-      return res.status(404).json({ message: 'Video not found' });
-    }
-
-    return res.status(200).json({ video: result[0], message: 'Video selected successfully' });
-  } catch (err) {
-    console.error('Video Selection error at /api/:video', err);
-    return res.status(500).json({ message: 'Something went wrong! Please try again.' });
-  }
-};
-
-module.exports = handleSelectedVideo;
-          as: 'creator',
-        },
-      },
-      { $unwind: '$creator' },
-      {
-        $addFields: {
-          // Ensure $creator.subscribers is an array
-          isSubscribed: {
-            $in: [userId, { $ifNull: ["$creator.subscribers", []] }]
-          },
-        },
-      },
-      {
-        $addFields: {
-          isLiked: {
-            $in: [userId, "$likes"]
-          }
-        }
-      },
-      {
-        $addFields: {
-          isUnliked: {
-            $in: [userId, "$unlikes"]
-          }
-        }
-      },
-      {
-        $project: {
-          _id: 1,
-          title: 1,
-          content: 1,
-          videoUrl: 1,
-          comments: 1,
-          userName: '$creator.userName',
-          ownerId: '$creator._id',
-          profile: '$creator.profile',
-          subscribersCount: { $size: '$creator.subscribers' },
-          isSubscribed: 1,
-          isLiked: 1,
-          isUnliked: 1,
-          likeCount: { $size: "$likes" },
-          dislikeCount: { $size: "$unlikes" },
-        },
       },
     ]);
 
