82b347c472869b1fe3e76b3268e2f30c1e38c6fc
Banoth Sujith <banothsujith4@gmail.com>
2025-03-23 17:47:34 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,9 +8,10 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-  origin:'*',
-
-  credentials: true,
+  origin: ["https://blog-front-end-gilt.vercel.app", "http://localhost:5173"], 
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
 }));
 app.use(express.json());
 app.use(cookieParser());
