8b99038b691eb0336082f513cfdc715d4d785bc5
BanothSujith <banothsujith4@gmail.com>
2025-04-05 21:33:06 +0530

    Update CORS configuration to restrict origins for enhanced security

--- server/index.js
+++ server/index.js
@@ -10,8 +10,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-  // origin: ["https://blog-front-end-gilt.vercel.app",  "http://localhost:5173","*"],
-origin: "*",
+origin: ["https://blog-front-end-gilt.vercel.app",  "http://localhost:5173"],
   credentials: true,
 }));
 app.use(express.json());
